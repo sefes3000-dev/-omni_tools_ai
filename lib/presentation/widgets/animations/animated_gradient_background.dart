2a50227bc994db968a97d598d006ad581e685423
sefes3000-dev <sefes3000@gmail.com>
2026-07-22 00:46:21 +0100

    Create animated_gradient_background.dart

---
+++ lib/presentation/widgets/animations/animated_gradient_background.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimatedGradientBackground extends StatefulWidget {
+  final Widget child;
+
+  const AnimatedGradientBackground({super.key, required this.child});
+
+  @override
+  State<AnimatedGradientBackground> createState() => _AnimatedGradientBackgroundState();
+}
+
+class _AnimatedGradientBackgroundState extends State<AnimatedGradientBackground>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: const [
+                Color(0xFF6366F1),
+                Color(0xFF8B5CF6),
+                Color(0xFFEC4899),
+              ],
+              transform: GradientRotation(_controller.value * 2 * math.pi),
+            ),
+          ),
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
