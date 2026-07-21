8f5bb6c731e00f1b1386a5b7ad983d7cb6083f05
sefes3000-dev <sefes3000@gmail.com>
2026-07-22 00:36:48 +0100

    Create failures.dart

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  final int? code;
+
+  const Failure(this.message, {this.code});
+
+  @override
+  List<Object?> get props => [message, code];
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message, {super.code});
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure(super.message);
+}
+
+class PermissionFailure extends Failure {
+  const PermissionFailure(super.message);
+}
+
+class ValidationFailure extends Failure {
+  const ValidationFailure(super.message);
+}
+
+class UnknownFailure extends Failure {
+  const UnknownFailure(super.message);
+}
