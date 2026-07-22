9cdd61b8a29cc7e3986a9b3696cddc5674f8cabf
sefes3000-dev <sefes3000@gmail.com>
2026-07-22 01:33:26 +0100

    Create barcode_scanner_screen.dart

---
+++ lib/presentation/screens/tools/lib/presentation/screens/tools/barcode_scanner_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BarcodeScannerScreen extends StatelessWidget {
+  const BarcodeScannerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Barcode Scanner')),
+      body: const Center(child: Text('Barcode Scanner - Implementation in progress')),
+    );
+  }
+}
